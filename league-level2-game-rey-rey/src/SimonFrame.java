16e575efd0c76cbe1dc90ac82d1df9dbd7d053f2
horndjellyfish <hornedjellyfish@gmail.com>
2020-04-04 11:56:25 -0700

    compiled runnable jar

---
+++ league-level2-game-rey-rey/src/SimonFrame.java
@@ -0,0 +1,71 @@
+
+//Frame
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SimonFrame extends JFrame  {
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 600;
+
+    SimonMenuPanel _menuPanel = new SimonMenuPanel(this);
+    SimonEndPanel _endPanel = new SimonEndPanel(this);
+
+    //SimonGamePanel _gamePanel = new SimonGamePanel(this);
+    SimonGamePanel2 _gamePanel = new SimonGamePanel2(this);
+
+    public CardLayout _layout = new CardLayout();
+
+    public SimonFrame() {
+        super("Simon Frame");
+        setup();
+        displayMenuPanel();
+        displayGamePanel();
+        displayEndPanel();
+    }
+
+    // configure and setup UI Frame
+    private void setup() {
+        this.setLayout(_layout);
+        this.addKeyListener(_menuPanel);
+
+        setVisible(true);
+        setSize(WIDTH, HEIGHT);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    // start the Game
+    public void startGame() {
+        // switch to Game Panel Card
+        _layout.next(this.getContentPane());
+
+        // Start Game Panel
+        _gamePanel.startGame();
+    }
+
+    // Stop the Game
+    public void stopGame( int score) {
+        // update the score
+        _endPanel.setScore( score );
+
+        // Switch to END Card UI
+        _layout.next(this.getContentPane());
+    }
+
+    //  Add Game Panel UI to Cards
+    private void displayGamePanel() {
+        this.add(_gamePanel);
+    }
+
+    //  Add Menu Panel UI to Cards
+    private void displayMenuPanel() {
+        this.add(_menuPanel);
+    }
+
+    //  Add End Panel UI to Cards
+    private void displayEndPanel() {
+        this.add(_endPanel);
+    }
+
+}
---
+++ league-level2-game-rey-rey/src/SimonMenuPanel.java
@@ -0,0 +1,52 @@
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class SimonMenuPanel extends JPanel implements KeyListener {
+
+    Font titleFont = new Font("Impact", Font.PLAIN, 120);
+    Font subtitleFont = new Font("Impact", Font.PLAIN, 40);
+    Font smaller = new Font("Impact", Font.PLAIN, 20);
+
+    SimonFrame _parent = null;
+
+    public SimonMenuPanel( SimonFrame parent)
+    {
+
+        _parent = parent;
+    }
+
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRect(0, 0, SimonFrame.WIDTH, SimonFrame.HEIGHT);
+        g.setColor(Color.white);
+        g.setFont(titleFont);
+        g.drawString("SIMON", 140, 300);
+        g.setFont(subtitleFont);
+        g.drawString("Press Enter to start", 150, 400);
+        g.setFont(smaller);
+        g.drawString("When the game starts, different colors will light up. When the color", 40, 470);
+        g.drawString("lights up, click that color. As time goes on, the pattern will get", 60, 495);
+        g.drawString("increasingly more complex. Don't press the wrong color!", 80, 520);
+    }
+
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+           _parent.startGame();
+        }
+    }
+
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
